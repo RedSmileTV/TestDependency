f754ba3ed29aa67da1705638df86a577deedaf83
RedSmile <lelledefeo@gmail.com>
2024-11-23 11:30:17 +0100

    0.3.0 release

--- src/main/java/de/redsmiletv/Test.java
+++ src/main/java/de/redsmiletv/Test.java
@@ -1,6 +1,23 @@
 package de.redsmiletv;
 
+/**
+ * The Calculator class provides methods to perform basic arithmetic operations
+ * such as addition, subtraction, multiplication, and division.
+ * <p>
+ * Example usage:
+ * <pre>
+ *     Calculator calculator = new Calculator();
+ *     int sum = calculator.add(5, 10);  // sum = 15
+ *     int diff = calculator.subtract(10, 5);  // diff = 5
+ * </pre>
+ * </p>
+ *
+ * @author RedSmileTV
+ * @version 1.0
+ */
+
 public class Test {
+
 
     private String name;
 
